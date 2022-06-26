346b82f54d23c4bf128cf8c4730715e75fd7fc4a
TheHabibi <celebialieren@gmail.com>
2022-06-27 01:12:45 +0200

    Created MovieBackdropCardView

---
+++ swiftui-tmdb/Views/MovieBackdropCard.swift
@@ -0,0 +1,42 @@
+//
+//  MovieBackdropCard.swift
+//  swiftui-tmdb
+//
+//  Created by Ali Eren Çelebi on 27.06.2022.
+//
+
+import SwiftUI
+
+
+struct MovieBackdropCard: View {
+    
+    let movie: Movie
+    @ObservedObject var imageLoader = ImageLoader()
+    
+    var body: some View {
+        VStack(alignment: .leading)
+        {
+            ZStack{
+                Rectangle()
+                    .fill(Color.gray.opacity(0.3))
+                
+                if self.imageLoader.image != nil {
+                    Image(uiImage: self.imageLoader.image!)
+                        .resizable()
+                }
+            }
+            .aspectRatio(16/9, contentMode: .fit)
+            .cornerRadius(8)
+            Text(movie.title)
+        }
+        .onAppear {
+            self.imageLoader.loadImage(with: self.movie.backdropURL)
+        }
+    }
+}
+
+struct MovieBackdropCard_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieBackdropCard(movie: Movie.stubbedMovie)
+    }
+}
