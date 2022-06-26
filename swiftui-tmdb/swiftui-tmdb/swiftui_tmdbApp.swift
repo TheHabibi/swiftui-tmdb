49e9486492a1253c3ef80f48f66cb1ae52ed94b5
TheHabibi <celebialieren@gmail.com>
2022-06-26 21:12:08 +0200

    Initial Commit

---
+++ swiftui-tmdb/swiftui-tmdb/swiftui_tmdbApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_tmdbApp.swift
+//  swiftui-tmdb
+//
+//  Created by Ali Eren Çelebi on 26.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_tmdbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
